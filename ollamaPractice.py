57120d81b0987188071647a1f2901883f3809d07
pranavperumal <pranavpperumal@gmail.com>
2025-06-23 18:54:51 -0700

    model verification works

--- ollamaPractice.py
+++ ollamaPractice.py
@@ -1,12 +1,19 @@
 import ollama
 
 models = ollama.list()
-
 model_name = 'deepseek-r1:8b'
 
-if model_name not in models:
-    print(f"The model that is used in this code {model_name} is NOT on your device")
-    raise RuntimeError(f"Model \"{model_name}\" not found. Halting execution.")
+modelAvailable = False
+
+for m in models['models']:
+    if m['model'] == model_name:
+        modelAvailable = True
+        break
+
+if not modelAvailable:
+    answer = input(f"The model that is used in this code {model_name} is NOT on your device, would you like to install it? (Y/N)")
+    if answer != "Y":
+        raise RuntimeError(f"Model \"{model_name}\" not found. Halting execution.")
 
 
 response = ollama.chat(
